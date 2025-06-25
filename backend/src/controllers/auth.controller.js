3d8017040b5bf2ef05f7459ed85036f891ecb435
sshhaahh <ameenfalak786@gmail.com>
2025-06-25 18:37:12 +0530

    b

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import cloudinary from "../lib/cloudinary.js";
 
 // SIGNUP
